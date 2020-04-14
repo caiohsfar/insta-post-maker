68734fac84baf9bf23ff141bb945d5796492ed54
Caio Farias <caio.farias@di2win.com>
2020-04-14 01:01:52 -0300

    feature: add scrapper and get subtopics

--- robots/input.js
+++ robots/input.js
@@ -1,27 +1,33 @@
 const readline = require('readline-sync')
-
-function robot() {
+const Scrapper = require('./scrapper')
+async function robot() {
   const content = {
     maximunSentences: 7,
   }
 
   content.searchTerm = askAndReturnSearchTerm()
-  content.prefix = askAndReturnPrefix()
+  
+  content.subTopic = await askAndReturnSubtopic(content.searchTerm)
+
   console.dir(content, { depht: false })
 
   function askAndReturnSearchTerm() {
-    return readline.question('Digite um termo do Wikipedia: ')
+    return readline.question('> [Input]: Digite um termo do Wikipedia: ')
   }
-  function askAndReturnPrefix() {
-    const prefixes = ['Quem é', 'O que é', 'A história de', 'Outro']
-    const selectedPrefixIndex = readline.keyInSelect(
-      prefixes,
-      'Escolha uma opção: '
-    )
-    if (selectedPrefixIndex === 3)
-      return readline.question('Digite uma opção diferente: ')
+  async function askAndReturnSubtopic(searchTerm) {
+    //Scrapper Get topics
+    //Put these in a array
+    //Show to user
+    const scrapper = new Scrapper()
+    const subtopics = await scrapper.getSubtopicsFromWikipedia(searchTerm)
+    //const prefixes = ['Quem é', 'O que é', 'A história de', 'Outro']
 
-    return prefixes[selectedPrefixIndex]
+    const selectedPrefixIndex = readline.keyInSelect(
+      subtopics,
+      '> [Input]: Escolha um tópico: '
+    )
+
+    return subtopics[selectedPrefixIndex]
   }
 }
 
