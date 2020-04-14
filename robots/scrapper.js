8089b5424025dc6e4718c6273c275184f344cf45
Caio Farias <caio.farias@di2win.com>
2020-04-14 19:27:39 -0300

    feature: get content text from wikipedia

--- robots/scrapper.js
+++ robots/scrapper.js
@@ -3,6 +3,9 @@ const chrome = require('selenium-webdriver/chrome')
 const { Builder, By, until } = require('selenium-webdriver')
 
 class Scrapper {
+  WAIT_TIMEOUT = 20000
+  WIKIPEDIA_URL = 'https://pt.wikipedia.org/wiki/'
+
   constructor() {
     const serviceBuilder = new chrome.ServiceBuilder(
       './webdriver/chromedriver.exe'
@@ -16,19 +19,47 @@ class Scrapper {
   }
 
   async getElementById(id) {
-    return await this.driver.wait(until.elementLocated(By.id(id)))
+    return await this.driver.wait(
+      until.elementLocated(By.id(id)),
+      this.WAIT_TIMEOUT
+    )
+  }
+
+  async getElementByXpath(xPath) {
+    return await this.driver.wait(
+      until.elementLocated(By.xpath(xPath)),
+      this.WAIT_TIMEOUT
+    )
+  }
+
+  async getParagraph(topic, subtopic) {
+    try {
+      await this.driver.get(this.WIKIPEDIA_URL + topic)
+
+      const spanId = subtopic.replace(/ /, '_')
+      const xPath = `//*[@id='${spanId}']/../following-sibling::p`
+
+      const paragraphElement = await this.getElementByXpath(xPath)
+      const sourceText = await paragraphElement.getText()
+
+      return sourceText
+    } catch (e) {
+      console.error('> [Erro]: ' + e)
+      process.exit(1)
+    } finally {
+      await this.driver.quit()
+    }
   }
 
   async getSubtopicsFromWikipedia(searchTerm) {
+    // TODO: HANDLE ERRORS
     console.log(
       `> [Scrapper]: Getting subtopics for term "${searchTerm}" on Wikipedia...`
     )
 
     try {
-      const url = `https://pt.wikipedia.org/wiki/${searchTerm}`
+      const url = this.WIKIPEDIA_URL + searchTerm
       await this.driver.get(url)
-      // TODO: GET INDICE
-
 
       const linkLi = await this.getElementById('ca-viewsource')
       await linkLi.findElement(By.tagName('a')).click()
@@ -46,6 +77,7 @@ class Scrapper {
       return subtopics
     } catch (e) {
       console.error('> [Erro]: ' + e)
+      process.exit(1)
     } finally {
       await this.driver.quit()
     }
